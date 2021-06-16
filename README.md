d29632c6cdcd3de44fddb8ef23b55858be606734
Chris De montigny <chrisdemontigny@yahoo.com>
2021-06-16 07:28:27 -0600

    square of squares

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # javascript-code-exercises
+
+<!-- this repo contains various javascript code exercises/katas from codewars.com. -->
---
+++ coding.js
@@ -0,0 +1,14 @@
+
+// ----------------------------------------------------------- //
+// Given an integral number, determine if it's a square number:
+
+// var isSquare = function(n) {
+//     if (Number.isInteger(Math.sqrt(n))===true){
+//       return true
+//     } else {
+//       return false
+//     }
+//   }
+  
+//   isSquare(2)
+// ----------------------------------------------------------- //
