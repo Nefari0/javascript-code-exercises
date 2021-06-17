80421962d9f5ba352a4ba3625157203dd47aa298
Chris De montigny <chrisdemontigny@yahoo.com>
2021-06-17 17:58:24 -0600

    summing a numbers digits

--- coding.js
+++ coding.js
@@ -12,3 +12,21 @@
   
 //   isSquare(2)
 // ----------------------------------------------------------- //
+
+// ----------------------------------------------------------- //
+// -- Summing a numbers digits -- //
+// Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
+
+// code //
+// function sumDigits(number) {
+//     var numStr = number.toString()
+//     var numArr = numStr.split('')
+//     var numSum = 0
+//     for (let i = 0; i < numArr.length; i++) {
+//       if (isNaN(numArr[i]) === false){numSum += parseInt(numArr[i])}
+//     }
+//     return(numSum)
+//   }
+  
+//   sumDigits(-32)
+// ----------------------------------------------------------- //
