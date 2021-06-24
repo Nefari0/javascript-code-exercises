0ad5cbd2e79fcf852d1e04f2ca0bde1d7905dade
Chris De montigny <chrisdemontigny@yahoo.com>
2021-06-24 17:19:15 -0600

    multiples of 3 or 5

--- coding.js
+++ coding.js
@@ -120,3 +120,28 @@
 
 // humanReadable(56456)
 // ----------------------------------------------------------- //
+
+
+// ----------------------------------------------------------- //
+//  -- multiples of 3 or 5 -- //
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+// Note: If the number is a multiple of both 3 and 5, only count it once. Also, if a number is negative, return 0(for languages that do have them)
+
+
+// const solution = (number) => {
+//   var threes = 0  
+//   var fives = 0
+
+//   for (let i = 0; i < number; i++){
+//     if(i % 3 === 0){threes += i}
+//     if(i % 5 === 0){fives += i}
+//     if(i % 5 === 0 && i % 3 === 0){fives -= i}
+
+//   }
+//   return(threes + fives)
+// }
+// ----------------------------------------------------------- //
