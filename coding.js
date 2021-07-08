d480baa2eb2b4b16b09536f716bc32eae70eb9d6
Chris De montigny <chrisdemontigny@yahoo.com>
2021-07-08 07:37:37 -0600

    Create Phone Number

--- coding.js
+++ coding.js
@@ -318,3 +318,20 @@
 // function disemvowel(str) { return str.replace(/[aeiou]/gi, '') }
 
 // ----------------------------------------------------------- //
+
+// ----------------------------------------------------------- //
+// -- Creat Phone Number -- //
+// Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+// example
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+
+// -- Solution -- //
+// const createPhoneNumber = (inputArray) => {
+//   var first = inputArray.splice(0,3).join('')
+//   var third = inputArray.splice(3,7).join('')
+//   var outputStr = `(${first}) ${inputArray.join('')}-${third}`
+//   return(outputStr)
+// }
+
+// ----------------------------------------------------------- //
