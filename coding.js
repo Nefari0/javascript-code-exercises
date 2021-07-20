5b16d7b4e595a58df23e4481aa5ce615208f838e
Chris De montigny <chrisdemontigny@yahoo.com>
2021-07-19 23:59:01 -0600

    anagrams

--- coding.js
+++ coding.js
@@ -457,3 +457,20 @@
   
 // }
 // ----------------------------------------------------------- //
+
+// ----------------------------------------------------------- //
+// -- Where my anagrams at? -- //
+// Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. You should return an array of all the anagrams or an empty array if there are none.
+
+// -- solution -- //
+// const anagrams = (word,words) => {
+//   var newWords = []
+//   words.forEach(element => {
+//   if (element.replace(/[^\w]/g,'').toLowerCase().split('').sort().join() === word.replace(/[^\w]/g,'').toLowerCase().split('').sort().join()) {
+//     newWords.push(element)
+//   }
+
+//   })
+//   return(newWords)
+// }
+// ----------------------------------------------------------- //
