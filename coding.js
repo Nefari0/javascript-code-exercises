d7b45748f281a1c06ea5dda124e2b4290f1c00fe
Chris De montigny <chrisdemontigny@yahoo.com>
2021-07-20 11:12:33 -0600

    Tic-Tac-Toe checker: completed code submitted to codewars.com

--- coding.js
+++ coding.js
@@ -478,37 +478,55 @@
 // ----------------------------------------------------------- //
 // --Tic-Tac-Toe Checker --//
 
+// If we were to set up a Tic-Tac-Toe game, we would want to know whether the board's current state is solved, wouldn't we? Our goal is to create a function that will check that for us!
+
+// Assume that the board comes in the form of a 3x3 array, where the value is 0 if a spot is empty, 1 if it is an "X", or 2 if it is an "O":
+
+// We want our function to return:
+
+//     -1 if the board is not yet finished AND no one has won yet (there are empty spots),
+//     1 if "X" won,
+//     2 if "O" won,
+//     0 if it's a cat's game (i.e. a draw).
+
+// You may assume that the board passed in is valid in the context of a game of Tic-Tac-Toe.
+
 // -- Solution -- //
 const isSolved = (board) => {
-  var horrizontalWinner = true
-  var diagonalWinner = true
-  var verticalWinner = true
+  var horizontalWinner = null
+  var diagonalWinner = null
+  var verticalWinner = null
   // check horrizontal
   for (let i = 0; i < board.length; i++){
     for (let j = 0; j < board.length; j++){
       var isSame = board[i].filter(element => element === j)
-    if (isSame.length === 3 && isSame[0] > 0) {
-      return(isSame[i])
-    } else {horrizontalWinner = false}
+    if (isSame.length === 3){
+      // return(isSame[i])
+      if (isSame[0] > 0){return(isSame[0])} else {return(-1)}
+    } else {horizontalWinner = false}
     }
   }
   // check diagonal
-  if(board[0][0] > 0 || board[0][2] > 0){
     if(board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
-      return(board[0][0])
+      if(board[0][0] > 0) {return(board[0][0])} else {return(-1)}
   }
     if(board[2][0] === board[1][1] && board[1][1] === board[0][2]) {
-      return(board[2][0])
+      if(board[2][0] > 0) {return(board[0][2])} else {return(-1)}
   } else {diagonalWinner = false}
-  }
+  
   // check vertical
   for (let i = 0; i < board.length; i++){
     if (board[0][i] === board[1][i] && board[1][i] === board[2][i]){
       return(board[0][i])
       } else {verticalWinner = false}
   }
-
-  if(horrizontalWinner === false && verticalWinner === false && diagonalWinner === false){return(-1)}
-
+  // check board for completion
+  for (let i = 0; i < board.length; i++){
+    if(board[i].includes(0) != false) {return (-1)}
+  }
+  // verify draw
+  if (horizontalWinner === false && verticalWinner === false && diagonalWinner === false){
+    return(0)
+  }
 }
 // ----------------------------------------------------------- //
