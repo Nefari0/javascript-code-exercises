0b8772ae22450fbd0d779c7327cabc9b24319197
Chris De montigny <chrisdemontigny@yahoo.com>
2021-07-29 22:22:19 -0600

    Take a Ten Minute Walk

--- coding.js
+++ coding.js
@@ -615,3 +615,24 @@ const formatDuration = (seconds) => {
   }
 
 // ----------------------------------------------------------- //
+
+// ----------------------------------------------------------- //
+// --- Take a Ten Minute Walk --- //
+
+// You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+// Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).
+
+// --- Solution --- //
+
+const isValidWalk = (walk) => {
+  distance = 10
+
+  var n = walk.filter(e => e === 'n').length
+  var s = walk.filter(e => e === 's').length
+  var e = walk.filter(e => e === 'e').length
+  var w = walk.filter(e => e === 'w').length
+
+  return(n+s+e+w === distance ? (e-w === 0 && n-s === 0 ? true : false) : false)
+
+}
